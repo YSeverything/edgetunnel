7eebdaa84d3481c008117f8b73a5b645f87e09aa
everything <136693659+YSeverything@users.noreply.github.com>
2025-01-15 19:39:44 +0800

    取消禁止测速

--- 明文源码.js
+++ 明文源码.js
@@ -44,7 +44,7 @@ let proxyIPPool = [];
 let path = '/?ed=2560';
 let 动态UUID;
 let link = [];
-let banHosts = [atob('c3BlZWQuY2xvdWRmbGFyZS5jb20=')];
+let banHosts = []; // 清空禁止列表 atob('c3BlZWQuY2xvdWRmbGFyZS5jb20=') //禁止speed.cloudflare.com测速
 //伪装网页
 async function nginx() {
 	const text = `
